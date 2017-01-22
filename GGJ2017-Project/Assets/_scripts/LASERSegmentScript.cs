16e5bcbeb0ce10a8a3760f8206eaff9890e6e8e0
Roy Kirk <ricotalzier@gmail.com>
2017-01-22 16:35:41 +1100

    laser reaycast fix

--- GGJ2017-Project/Assets/_scripts/LASERSegmentScript.cs
+++ GGJ2017-Project/Assets/_scripts/LASERSegmentScript.cs
@@ -161,11 +161,11 @@ public class LASERSegmentScript : MonoBehaviour {
                 {
                     CollisionChecks(hit.collider);
                 }
-                if (Physics.Raycast(transform.position + new Vector3(0, 0, -0.5f), new Vector3(0, 0, 1), out hit, 1.0f))
+                if (Physics.Raycast(transform.position + new Vector3(0, 0, -0.5f), new Vector3(1, 0, 0), out hit, 1.0f))
                 {
                     CollisionChecks(hit.collider);
                 }
-                if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), new Vector3(0, 0, 1), out hit, 1.0f))
+                if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), new Vector3(1, 0, 0), out hit, 1.0f))
                 {
                     CollisionChecks(hit.collider);
                 }
@@ -177,11 +177,11 @@ public class LASERSegmentScript : MonoBehaviour {
                 {
                     CollisionChecks(hit.collider);
                 }
-                if (Physics.Raycast(transform.position + new Vector3(0, 0, -0.5f), new Vector3(0, 0, 1), out hit, 1.0f))
+                if (Physics.Raycast(transform.position + new Vector3(0, 0, -0.5f), new Vector3(1, 0, 0), out hit, 1.0f))
                 {
                     CollisionChecks(hit.collider);
                 }
-                if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), new Vector3(0, 0, 1), out hit, 1.0f))
+                if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), new Vector3(1, 0, 0), out hit, 1.0f))
                 {
                     CollisionChecks(hit.collider);
                 }
