3c11710c25b1f87fec4771a2fbb27fa3f6594cf4
Zac <zac.molyneux@hotmail.com>
2017-01-22 15:38:56 +1100

    added a bunch of art assets

--- GGJ2017-Project/Assets/_scripts/HiveMovement.cs
+++ GGJ2017-Project/Assets/_scripts/HiveMovement.cs
@@ -4,8 +4,14 @@ using System.Collections;
 public class HiveMovement : MonoBehaviour
 {
     public float speed = 0.2f;
-	// Use this for initialization
-	void Start ()
+
+    public bool left;
+    public bool right;
+    public bool up;
+    public bool down;
+
+    // Use this for initialization
+    void Start ()
     {
 	    
 	}
@@ -13,7 +19,7 @@ public class HiveMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-	    if(Input.GetKey(KeyCode.A))
+	    if(Input.GetKey(KeyCode.A) && !left)
         {
             transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
         }
@@ -28,6 +34,47 @@ public class HiveMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+        }
+
+
+        //raycasts to detect collisions with walls
+
+        RaycastHit hit;
+
+        //left middle
+        if(Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z), new Vector3(-1, 0, 0), out hit, 1.6f))
+        {
+            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z), new Vector3(-1, 0, 0));
+            left = true;
+            Debug.Log(hit.transform.name);
+        }
+        else
+        {
+            left = false;
+        }
+
+        //left top
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z + 1.5f), new Vector3(-1, 0, 0), out hit, 1.6f))
+        {
+            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z + 1.5f), new Vector3(-1, 0, 0));
+            left = true;
+            Debug.Log(hit.transform.name);
+        }
+        else
+        {
+            left = false;
+        }
+
+        //left bottom
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z - 1.5f), new Vector3(-1, 0, 0), out hit, 1.6f))
+        {
+            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z), new Vector3(-1, 0, 0));
+            left = true;
+            Debug.Log(hit.transform.name);
+        }
+        else
+        {
+            left = false;
         }
     }
 }
