97af2ade20821089332520db490eec4095b2af7a
Roy Kirk <ricotalzier@gmail.com>
2017-01-22 12:50:48 +1100

    screen shake and new laser models

---
+++ GGJ2017-Project/Assets/ScreenShakeScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShakeScript : MonoBehaviour {
+
+    float shake = 0.0f;
+    public float shakeAmount = 0.1f;
+    public float shakeDuration = 0.3f;
+    Camera camera;
+    Vector3 initpos;
+    // Use this for initialization
+    void Start () {
+        camera = gameObject.GetComponent<Camera>();
+        initpos = camera.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (shake > 0)
+        {
+            camera.transform.localPosition = initpos + Random.insideUnitSphere * shakeAmount;
+            shake -= Time.deltaTime;
+        }
+        else
+        {
+            camera.transform.localPosition = initpos;
+        }
+    }
+
+    public void Shake()
+    {
+        shake = shakeDuration;
+    }
+}
--- GGJ2017-Project/Assets/_scripts/LaserSpawnScript.cs
+++ GGJ2017-Project/Assets/_scripts/LaserSpawnScript.cs
@@ -47,9 +47,9 @@ public class LaserSpawnScript : LASERSegmentScript {
 
         //script new lasers
         spawns.Add(new LaserSpawn(W, yellow, 1, 1, 5f));
-        spawns.Add(new LaserSpawn(W, yellow, 1, 1, 6f));
-        spawns.Add(new LaserSpawn(W, red, 1, 1, 7f));
-        spawns.Add(new LaserSpawn(W, blue, 1, 1, 8f));
+        spawns.Add(new LaserSpawn(E, yellow, 1, 1, 6f));
+        spawns.Add(new LaserSpawn(S, red, 1, 1, 7f));
+        spawns.Add(new LaserSpawn(N, blue, 1, 1, 8f));
         spawns.Add(new LaserSpawn(W, purple, 1, 2, 9f));
         //spawns.Add(new LaserSpawn(E, green, 1, 1, 43.7f));
 
