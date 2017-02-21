21fed9a96bdec5f3cbd5ba13ef1e088ecf858383
Zac <zac.molyneux@hotmail.com>
2017-02-21 14:35:38 +1100

    added animated menu letters.

---
+++ GGJ2017-Project/Colour Theory Files/Colour Theory/Assets/_Scripts/LetterFeedback.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LetterFeedback : MonoBehaviour
+{
+    public bool animate;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+
+    bool expand = true;
+
+    float increment;
+
+    Vector3 scaleMod = new Vector3(0, 10, 0);
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(animate)
+        {
+            if(expand)
+            {
+                //transform.localScale += new Vector3(0, 1, 0) * Time.deltaTime;
+            }
+            else
+            {
+                transform.localScale += new Vector3(0, -1, 0) * Time.deltaTime;
+            }
+            
+        }
+	}
+}
