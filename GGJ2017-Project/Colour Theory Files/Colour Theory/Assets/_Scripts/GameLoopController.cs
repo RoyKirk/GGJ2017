c194b1b53376c41dbbbab017a4a17f8f5bde701b
Zac <zac.molyneux@hotmail.com>
2017-03-01 14:12:33 +1100

    fixed so anyone can start the game by pressing back.

--- GGJ2017-Project/Colour Theory Files/Colour Theory/Assets/_Scripts/GameLoopController.cs
+++ GGJ2017-Project/Colour Theory Files/Colour Theory/Assets/_Scripts/GameLoopController.cs
@@ -13,17 +13,21 @@ public class GameLoopController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        Players = GameObject.FindGameObjectsWithTag("Player");
-        if(Players.Length <= 1)
-        {
-            Application.LoadLevel(1);
-        }
+        //Players = GameObject.FindGameObjectsWithTag("Player");
+        //if(Players.Length <= 1)
+        //{
+        //    Application.LoadLevel(1);
+        //}
 
-        for(int i =0; i < Players.Length;i++)
+        for(int i = 0; i < Controller.connected.Length; i++)
         {
             if(Controller.state[i].Buttons.Start == XInputDotNetPure.ButtonState.Pressed && Controller.connected[i] == true)
             {
                 Application.LoadLevel(0);
+            }
+            if(Controller.state[i].Buttons.Back == XInputDotNetPure.ButtonState.Pressed && Controller.connected[i] == true)
+            {
+                Application.LoadLevel(1);
             }
         }
 	}
