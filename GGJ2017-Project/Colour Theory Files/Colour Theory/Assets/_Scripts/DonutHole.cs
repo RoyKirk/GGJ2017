7890a306fe03785d2ebc4020d9cfd8970afe3d9c
Zac <zac.molyneux@hotmail.com>
2017-04-11 16:35:25 +1000

    added donuthole.

---
+++ GGJ2017-Project/Colour Theory Files/Colour Theory/Assets/_Scripts/DonutHole.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DonutHole : MonoBehaviour
+{
+    public float sizeScale = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.localScale += new Vector3(((Time.deltaTime * 0.01f) + 0.001f) / sizeScale, 0, ((Time.deltaTime * 0.01f) + 0.001f )/ sizeScale);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
